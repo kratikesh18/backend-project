8ceefae4098dcaaf5fcf8405a45ae51538649226
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-13 06:47:34 +0530

    modified some styling

--- src/components/ConstructionPage.jsx
+++ src/components/ConstructionPage.jsx
@@ -5,24 +5,28 @@ import React from "react";
 
 function ConstructionPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-500">
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-[#040204]">
       <img
-        src="\public\Construction..png"
+        src="https://media1.tenor.com/m/kbbJOE4dIJkAAAAC/working-on.gif"
         alt="Under Construction"
-        className="mb-8 w-[25vw]"
+        className="mb-8 md:w-[25vw]"
         style={{ maxWidth: "100%", height: "auto" }}
       />
+
+<div className="flex flex-col relative bottom-[6rem] text-center justify-center  items-center">
+
       <h1 className="text-4xl font-bold mb-4"> Project Under Construction </h1>
-      <p className="text-lg text-gray-300 mb-8"> developer 
+      <p className="text-lg text-gray-200 mb-8"> Developer 
         <span className="font-extrabold"> Kartikesh</span> is working hard to bring you an awesome project. Check back soon!
       </p>
-      <div className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-blue-600 transition duration-300">
+      <div className="w-fit bg-blue-800 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-blue-950 transition duration-20000">
         <a href="https://github.com/kratikesh18/Javascript-Backend.git" className=" text-white hover:text-white transform-none">
         Source Code 
         </a>
         
-      </div>
+        </div>
       <p className="mt-4">I'm happy to recive contributions</p>
+</div>
     </div>
   );
 }
